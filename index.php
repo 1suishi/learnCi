cb5bfa6063fa7b793d992cff48d703916be10ae3
benniu <1962323664@qq.com>
2015-05-31 15:07:33 +0800

    add class and method

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * --------------------------------------------------------------------
- * 获取 uri ，并通过 uri 调用相应的方法
+ * get  uri ，and acording uri invoke  corresponding method
  * --------------------------------------------------------------------
  */
 
@@ -10,22 +10,53 @@ function detect_uri() {
     if ( ! isset($_SERVER['REQUEST_URI']) OR ! isset($_SERVER['SCRIPT_NAME'])) {
         return '';
     }
+	
+	
+
 
     $uri = $_SERVER['REQUEST_URI'];	
     if (strpos($uri, $_SERVER['SCRIPT_NAME']) === 0) {
         $uri = substr($uri, strlen($_SERVER['SCRIPT_NAME']));
     }
-
+	
     if ($uri == '/' || empty($uri)) {
         return '/';
     }
 
     $uri = parse_url($uri, PHP_URL_PATH);
-
-
-    // 将路径中的 '//' 或 '../' 等进行清理
+    // deal with  '//' or '../' 
     return str_replace(array('//', '../'), '/', trim($uri, '/'));
 }
 
+
+function explode_uri($uri) {
+
+    foreach (explode('/', preg_replace("|/*(.+?)/*$|", "\\1", $uri)) as $val) {
+        $val = trim($val);
+        if ($val != '') {
+            $segments[] = $val;
+        }
+    }
+    return $segments;
+}
+
+class Welcome {
+
+	function hello() {
+		echo 'My first Php Framework!';
+	}
+}
+
 $uri = detect_uri();
-echo $uri;
+$uri_segments = explode_uri($uri);
+
+$class = $uri_segments[0];
+$method = $uri_segments[1];
+
+
+// invoke class and method
+$CI = new $class();
+ 
+//echo detect_uri(); 
+$CI->$method();
+
